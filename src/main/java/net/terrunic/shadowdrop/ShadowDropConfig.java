58ce572ac5b14d5e900a191bbd97d7ba4a99136d
Terrunic <terrunic@gmail.com>
2026-03-28 18:44:45 +0000

    Rewrite to change mixin from GuiGraphics.renderItem to ItemRenderer.render, now applying shadows to all items rendered with a GUI ItemDisplayContext; also reworked config for more shadow/crop customisation

--- src/main/java/net/terrunic/shadowdrop/ShadowDropConfig.java
+++ src/main/java/net/terrunic/shadowdrop/ShadowDropConfig.java
@@ -30,12 +30,17 @@ public class ShadowDropConfig
     {
         // Config values
         public final ForgeConfigSpec.BooleanValue modEnabled;
+        public final ForgeConfigSpec.ConfigValue<String> shadowColor;
         public final ForgeConfigSpec.IntValue shadowAlpha;
         public final ForgeConfigSpec.IntValue shadowXOffset;
         public final ForgeConfigSpec.IntValue shadowYOffset;
-        public final ForgeConfigSpec.BooleanValue onlySlotShadows;
-        public final ForgeConfigSpec.BooleanValue cropToHotbarSlots;
-        public final ForgeConfigSpec.BooleanValue cropToVisualSlots;
+        public final ForgeConfigSpec.IntValue shadowZOffset;
+        public final ForgeConfigSpec.BooleanValue shadowsAlways;
+        public final ForgeConfigSpec.BooleanValue shadowsInSlots;
+        public final ForgeConfigSpec.BooleanValue shadowsInHotbar;
+        public final ForgeConfigSpec.BooleanValue shadowsInCursor;
+        public final ForgeConfigSpec.BooleanValue cropToSlots;
+        public final ForgeConfigSpec.BooleanValue cropToHotbar;
         public final ForgeConfigSpec.ConfigValue<List<? extends String>> slotBrColors;
         public final ForgeConfigSpec.ConfigValue<List<? extends String>> translucentItems;
         public final ForgeConfigSpec.ConfigValue<List<? extends String>> transparentItems;
@@ -43,12 +48,19 @@ public class ShadowDropConfig
         public Client(ForgeConfigSpec.Builder builder)
         {
             builder.comment(" Shadow Drop: Client Configuration")
+                .comment(" (note: for EMI compatibility, set 'use-batched-renderer' to 'false' in EMI config)")
                 .comment("")
                 .push("general");
 
             modEnabled = builder
                 .comment(" Enable/disable the mod entirely")
                 .define("modEnabled", true);
+
+            shadowColor = builder
+                .comment("")
+                .comment(" Shadow color (RGB hex)")
+                .define("shadowColor","#000000",
+                    obj -> obj instanceof String && ((String) obj).matches("^#[0-9A-Fa-f]{6}$"));
 
             shadowAlpha = builder
                 .comment("")
@@ -65,27 +77,52 @@ public class ShadowDropConfig
                 .comment(" Shadow Y offset")
                 .defineInRange("shadowYOffset", 1, 0, 4);
 
+            shadowZOffset = builder
+                .comment("")
+                .comment(" Shadow Z offset")
+                .defineInRange("shadowZOffset", -50, -100, 0);
+
             builder.pop();
-            builder.push("cropping");
+            builder.push("shadowContexts");
 
-            cropToHotbarSlots = builder
-                .comment(" Crop shadows to hotbar slots (regardless of hotbar color)?")
-                .define("cropToHotbarSlots", true);
+            shadowsAlways = builder
+                .comment(" Items always have shadows (overrules other shadow contexts when true)")
+                .define("shadowsAlways", true);
 
-            cropToVisualSlots = builder
+            shadowsInSlots = builder
                 .comment("")
-                .comment(" Crop shadows to visual slots (determined by slotBrColors)?")
-                .define("cropToVisualSlots", true);
+                .comment(" Items in slots (see slot detection config) have shadows")
+                .define("shadowsInSlots", true);
 
-            onlySlotShadows = builder
+            shadowsInHotbar = builder
                 .comment("")
-                .comment(" Only give shadows to items in visual slots (including the hotbar and items picked up by cursor)?")
-                .define("onlySlotShadows", false);
+                .comment(" Items in hotbar slots always have shadows")
+                .define("shadowsInHotbar", true);
+
+            shadowsInCursor = builder
+                .comment("")
+                .comment(" Items being carried by the cursor have shadows")
+                .define("shadowsInCursor", true);
+
+            builder.pop();
+            builder.push("cropContexts");
+
+            cropToSlots = builder
+                .comment(" Shadows of items in slots (see slot detection config) should be cropped")
+                .define("cropToSlots", true);
+
+            cropToHotbar = builder
+                .comment("")
+                .comment(" Shadows of items in the hotbar should always be cropped")
+                .define("cropToHotbar", true);
+
+            builder.pop();
+            builder.push("slotDetection");
 
             slotBrColors = builder
-                .comment("")
                 .comment(" RGB hex colors for detection of visual slots (e.g. \"#FFFFFF\")")
                 .comment(" This color is searched for in the bottom-right corner pixel wherever an item is rendered")
+                .comment(" Adjust for compatibility with resource packs or modded GUIs where this pixel is not #FFFFFF")
                 .defineList("slotBrColors",
                     List.of("#FFFFFF"),
                     obj -> obj instanceof String && ((String) obj).matches("^#[0-9A-Fa-f]{6}$"));
